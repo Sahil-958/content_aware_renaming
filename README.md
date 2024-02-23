21b0305b588ad2fa45fe4519aa0394a973932ec7
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-02-23 11:21:22 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,143 @@
+# Image Processing with Microsoft Vision API
+
+This Bash script facilitates image processing using the Microsoft Vision API. It analyzes images and generates captions based on their content. The script can handle single images or process all images within a specified directory.
+
+
+
+## Here is Some work done by it 
+|| |
+|---------|---------|
+| ![Example01](assets/example01.png) | ![Example02](assets/example02.png) |
+| *Grouped 1* | *Grouped 2* |
+| ![Example03](assets/a_black_and_white_drawing_of_a_person_with_a_blindfold.png) | ![Example04](assets/a_white_drawing_of_a_person_with_a_mask_on_his_head.png) |
+| *a_black_and_white_drawing_of_a_person_with_a_blindfold*| *a_white_drawing_of_a_person_with_a_mask_on_his_head* |
+
+
+### Logs Structure
+
+- Sceneraio 1:
+    ```
+    Error: File '/home/sawhill/walls/dharmx_walls/nature/no_image_here.png' does not exist.
+    No image files found in directory and it's sub directories: /home/sawhill/Templates/
+    ```
+
+- Sceneraio 2:
+
+    ```
+    Renaming [img1.png] to [a_colorful_sky_with_clouds.png]
+    Renaming [img2.jpg] to [a_mountain_with_snow_on_it.jpg]
+    Renaming [img3.png] to [a_person_sitting_on_a_horse_next_to_a_lamp_post.png]
+    Renaming [wallheaven.png] to [a_tower_in_a_forest.png]
+    Renaming [wall.png] to [a_yellow_moon_in_the_sky.png]
+    ```
+
+- Sceneraio 3:
+
+    ```
+    Old name:[a_person_playing_a_guitar.png] == new name:[a_person_playing_a_guitar.png] Skipped
+    Old name:[a_stone_walkway_leading_to_a_lighthouse.jpg] == new name:[a_stone_walkway_leading_to_a_lighthouse.jpg] Skipped
+    ```
+
+- Sceneraio 4:
+
+    ```
+    Skipping image [/home/sawhill/wall/remote/unsorted/a_bicycle_parked_on_a_street.jpg]
+    Reason: Access denied due to invalid subscription key or wrong API endpoint. Make sure to provide a valid key for an active subscription and use a correct regional API endpoint for your resource. 
+    Skipping image [/home/sawhill/wall/remote/monochrome/a_black_rose_with_a_black_background.jpg]
+    Reason: Access denied due to invalid subscription key or wrong API endpoint. Make sure to provide a valid key for an active subscription and use a correct regional API endpoint for your resource. 
+    ```
+
+
+
+## Prerequisites
+
+Before using this script, ensure you have the following:
+
+1. **Microsoft Vision API Key**: Obtain an API key from the Microsoft Azure portal. This key is required for accessing the Microsoft Vision API.
+
+2. **Microsoft Vision API Endpoint**: You need the URL of the Microsoft Vision API endpoint. This endpoint will be used to send requests for image analysis.
+
+3. **Bash Environment**: The script is written in Bash, so make sure you have a Bash-compatible environment available. It should work on most Unix-like operating systems, including Linux and macOS.
+
+4. **jq**: The script utilizes `jq` for parsing JSON responses from the Microsoft Vision API. Ensure `jq` is installed on your system. You can install it via package managers like `apt`, `brew`, or by downloading it from the official website.
+
+## Getting Started
+
+Follow these steps to use the script:
+
+1. **Get the Script**: You can directly copy pase or use below command.
+
+    ```bash
+    curl -fsSL https://raw.githubusercontent.com/Sahil-958/content_aware_renaming/main/renamer.sh > renamer.sh && chmod +x renamer.sh
+    ```
+
+2. **Verify the contents**: Verify the contents of script.
+
+    ```bash
+    cat renamer.sh | less
+    ```
+
+3. **Set up API Key and Endpoint**:  You can Speicfy that in three ways:   
+
+    - Open the script (`renamer.sh`) in a text editor. Replace the placeholders for `MICROSOFT_VISION_API_KEY` and `MICROSOFT_VISION_API_ENDPOINT` with your actual API key and endpoint URL.
+
+    - Use the `-endpoint` and `-key` Flags to speicfy the api endpoint and key 
+      
+    - Use the `-endpiont` and `-kf` Flags to specify key and endpoint the diff between `-kf` and `-key` is the `-kf` just let you speicfy that file for api key so the key doen't goes in your bash history and eliminate a potential risk of it might get stolen
+
+> [!CAUTION]
+> When you use -key flag you are giving api key as plain text which gets stored in you shell history
+
+4. **Run the Script**: Execute the script with appropriate options based on your requirements. See the usage section below for details on available options.
+
+    ```bash
+    ./renamer.sh [options] <image_directory>
+    ```
+
+## Usage
+
+The script supports various command-line options:
+
+- `-h`: Print usage information.
+- `-key <API_KEY>`: Set the Microsoft Vision API key.
+- `-endpoint <ENDPOINT_URL>`: Set the Microsoft Vision API endpoint URL.
+- `-p <NUM>`: Set the concurrency level (maximum number of images to process concurrently). Default is 3.
+- `-r <RESPONSE_FILE>`: Save API responses to a file.
+- `-l <LOG_FILE>`: Save logs to a file.
+- `-sr <REPLACEMENT>`: Specify a replacement character for spaces in image names.
+- `-sf <SINGLE_FILE>`: Process a single image file instead of a directory.
+
+Example usage:
+
+```bash
+./renamer.sh -p 5 -sr _ -r responses.txt -l logs.txt -key api_key.txt -endpoint "https://example.cognitiveservices.azure.com" ~/Pictures/
+```
+
+> [!NOTE]  
+> - The script will decends to all the subdirs inside a dir to fetch all the images 
+> - The max size of images supported by Microsoft is 20.97 MB 
+> - The Vision Api works best with natural images
+> - Set concurrency to low if the using free tire as only 20 class per minutes is available for free tier
+
+> [!CAUTION]
+> It uploads the images to Microsoft servers, do not use it with sensitive images. (or use it with caution) 
+
+## Credits and Improvements
+
+Originally inspired from https://github.com/sanjujosh/auto-image-renamer/ which is written in python.
+
+While the core functionality remains the same, several improvements have been made in this version:
+
+- **Added Support for Single Image Processing**: You can now process a single image file instead of a directory.
+- **Enhanced Concurrency Control**: Improved concurrency management for efficient processing of multiple images concurrently.
+- **Extended Error Handling**: Enhanced error handling for better resilience and stability.
+- **Improved Documentation**: Enhanced README.md file with detailed instructions, usage examples, and additional information.
+- **Added API Key Privacy Option**: Option to read the API key from a separate file for improved security, particularly in screen recording scenarios and active bash histories.
+- **Added Multiple API Version**: Option to select api versions and modes. Note: not available through flags
+- **Added Response and Log File Options**: Ability to save API responses and logs to specified files for better analysis and debugging.
+
+## Additional Information
+
+- **API Documentation**: You can find detailed documentation for the Microsoft Vision API [here](https://eastus.dev.cognitive.microsoft.com/docs/services/Cognitive_Services_Unified_Vision_API_2023-10-01/operations/61d65934cd35050c20f73ab6).
+
+These improvements aim to enhance the usability, performance, and security of the script. Contributions and feedback are always welcome!
---
+++ renamer.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+
+# Removed Azure resources: 
+# The Microsoft Vision API key and endpoint have been removed. 
+# Please replace the following variables with your own:
+MICROSOFT_VISION_API_KEY='22d12becea494ddb8e3a8544cb858b50'
+MICROSOFT_VISION_API_ENDPOINT='https://basher.cognitiveservices.azure.com'
+
+# Maximum number of parallel processes
+MAX_PARALLEL_JOBS=3
+
+# Get terminal width
+TERM_WIDTH=$(tput cols)
+
+ALLOWED_IMAGE_EXTENSIONS=('jpeg' 'jpg' 'png')
+logs=$(mktemp)
+
+rename_img() {
+    local old="$1"
+    local new="$2"
+    if [ "${new#ERROR_}" != "$new" ]; then
+        # String starts with "ERROR_"
+        echo -e "Skipping image [$old]\nReason: ${new#ERROR_} "
+        return
+    fi
+    local old_name=$(basename $old)
+    local base_dir=$(dirname $old)
+    local ext=${old##*.}
+    local new_name="$new.$ext"
+    local counter=1
+    if [ "$base_dir/$new_name" = "$old" ]; then
+        echo -e "   Old name:[$old_name] == new name:[$new_name] Skipped"
+        return 1
+    fi
+    while [ -e "$base_dir/$new_name" ]; do
+        if [ $counter -lt 10 ]; then
+            padded_counter="0$counter"  # Add padding for single-digit numbers
+        else
+            padded_counter="$counter"
+        fi
+        new_name="${new}_$padded_counter.$ext"
+        counter=$((counter + 1))
+    done
+
+    mv "$old" "$base_dir/$new_name"
+    echo "Renaming [$old_name] to [$new_name]"
+}
+
+get_caption() {
+    local image_file="$1"
+    local api_version="${2:-v4.0_caption}"  # Default to v4.0_caption if no version is provided
+    
+    case $api_version in
+        "v3.2")
+            local request_url="$MICROSOFT_VISION_API_ENDPOINT/vision/v3.2/describe?maxCandidates=1&language=en&model-version=latest"
+            local filter=".description.captions[0].text"
+            ;;
+        "v4.0_caption")
+            local request_url="$MICROSOFT_VISION_API_ENDPOINT/computervision/imageanalysis:analyze?api-version=2023-10-01&features=caption&model-version=latest&language=en"
+            local filter=".captionResult.text"
+            ;;
+        "v4.0_denseCaptions")
+            local request_url="$MICROSOFT_VISION_API_ENDPOINT/computervision/imageanalysis:analyze?api-version=2023-10-01&features=denseCaptions&model-version=latest&language=en"
+            local filter=".denseCaptionsResult.values[0].text"
+            ;;
+        *)
+            echo "Invalid API version. Please specify either 'v3.2', 'v4.0_caption', or 'v4.0_denseCaptions'."
+            exit 1
+            ;;
+    esac 
+
+    local response=$(curl -s -X POST -H "Ocp-Apim-Subscription-Key: $MICROSOFT_VISION_API_KEY" -H "Content-Type: application/octet-stream" --data-binary @"$image_file" "$request_url")
+    local caption_text=$( echo "$response" | jq -er "$filter // (\"ERROR_\" + .error.message)" )
+    
+    if [[ -n "$response_file" ]]; then
+        echo "$response" >> "$response_file"
+    fi 
+ 
+    if [ -n "$space_replacement" ] && [ "${new#ERROR_}" != "$new" ]; then
+    caption_text="${caption_text// /$space_replacement}"        
+    fi 
+
+    echo "$caption_text"
+}
+
+process_image() {
+   if [ $(stat -c %s "$1") -le 20971520 ]; then
+        local caption=$(get_caption "$1")
+        local result=$(rename_img "$1" "$caption")
+        echo "$result" >> $logs
+    else
+        echo "The specified file [$1] is not under 20,971,520 bytes (20.97MB) Please reduce the Size." >> $logs
+    fi
+}
+
+init() {
+    if [ -n "$single_file" ]; then
+        if [ -f "$single_file" ]; then
+            # Get the file extension
+            file_extension="${single_file##*.}"
+            file_extension="${file_extension,,}"  # Convert to lowercase for case-insensitive comparison
+            # Check if the file extension indicates an image file
+            if [[ "$file_extension" == "png" || "$file_extension" == "jpg" || "$file_extension" == "jpeg" ]]; then
+                echo "Processing Single image file: $single_file"
+                process_image "$single_file"
+            else
+                echo "Skipping non-image file: $single_file" >> $logs
+            fi
+        else
+            echo "Error: File '$single_file' does not exist." >> $logs
+        fi
+    fi
+    
+    if [ -n "$dir" ]; then
+        if [ -d "$dir" ]; then
+            echo "Max Parallel Jobs is: $MAX_PARALLEL_JOBS"
+            IFS=$'\n'
+            local images=()
+            readarray -t images < <(find "$dir" -type f -regex ".*\.\(jpg\|jpeg\|png\)")
+            if [ ${#images[@]} -eq 0 ]; then
+                echo "No image files found in directory and it's sub directories: $dir" >> $logs
+                return
+            fi
+            local count=0
+            for image in "${images[@]}"; do
+                if [ "$count" -lt "$MAX_PARALLEL_JOBS" ]; then
+                    ((count++))
+                    echo "Processing image - $image"
+                    process_image "$image" &
+                else
+                    echo "Maximum parallel jobs reached. Waiting for a free slot to process image - $image"
+                    wait -n
+                    ((count--))
+                    echo "Processing image - $image"
+                    process_image "$image" &
+                    ((count++))
+                fi
+            done
+            wait
+        else
+            echo "Error: Directory '$dir' does not exist." >> $logs
+            return
+        fi
+    else 
+        echo "Error: No valid input provided." >> $logs
+        usage
+        return
+    fi
+}
+
+
+usage() {
+    echo "Usage: $0 [options] <image1 dir>"
+    echo "Options:"
+    echo "  -h Prints this useage"
+    echo "  -key API key for vision api ex: '22d12becea494ddb8e3a8544cb858b50'"
+    echo "  -kf Same as the -key flag but uses file as input for api key (To hide the key in screen recording scenarios)"
+    echo "  -endpoint Url of the vision api endpoint ex: 'https://basher.cognitiveservices.azure.com'"
+    echo "  -p Set Concurrency Level How many image to process parallely (default: 3)"
+    echo "  -r Accpets a file name to save the responses from api"
+    echo "  -l Accpets a file name to save the logs"
+    echo "  -sr By default names have spaces in them so use -sr flag to send a space replacement like _ or -"
+    echo "  -sf Accept a single file instead of a dir"
+    echo "Example:"
+    echo "$0 -p 5 -sr _ -r responses.txt -kf api_key.txt -endpoint \"https://basher.cognitiveservices.azure.com\" ~/Pictures/"
+   exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+       -p)
+            shift
+            MAX_PARALLEL_JOBS=$1
+            ;;
+       -sf)
+            shift
+            single_file="$1"
+            ;;
+       -sr)
+            shift
+            space_replacement="$1"
+            ;;
+       -l)
+            shift
+            log_file=$1
+            ;;
+       -r)
+            shift
+            response_file=$1
+            ;;
+       -kf)
+            shift
+            MICROSOFT_VISION_API_KEY=$(cat $1)
+            ;;
+       -key)
+            shift
+            MICROSOFT_VISION_API_KEY="$1"
+            ;;
+       -endpoint)
+            shift
+            MICROSOFT_VISION_API_ENDPOINT="$1"
+            ;;
+       -h)
+            usage
+            ;;
+       -*)
+            echo "Unrecognized option: $1" >&2
+            usage
+            ;;
+       *)
+            dir="$1"
+            ;;
+    esac
+    shift
+done
+
+init
+echo -e "\nLogs:"
+# Print underscores equal to terminal width
+printf "%-${TERM_WIDTH}s\n" "_" | tr ' ' '_'
+cat $logs
+# Print underscores equal to terminal width
+printf "%-${TERM_WIDTH}s\n" "_" | tr ' ' '_'
+
+if [[ -n "$log_file" ]]; then
+    cat $logs > "$log_file"
+fi 
+rm $logs
